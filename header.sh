46ed44069828c5adf82e0b7f5e78a664035af2d1
Paul Millar <paul.millar@desy.de>
2019-10-16 13:52:05 +0200

    Update web-page headers in preparation for 6.0 release

--- header.sh
+++ header.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #  The next (currently unreleased) version of dCache
-NEXT_RELEASE=6.0
+NEXT_RELEASE=6.1
 
 #  Path within the filesystem corresponding to the web-server's root
 WEB_ROOT="../.."
